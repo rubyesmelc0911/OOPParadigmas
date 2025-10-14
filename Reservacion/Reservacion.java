65770f1bbaa9b6a0d089d8f89e8204499bd182e7
rubyesmelc0911 <rubyesme.lc0911@gmail.com>
2025-10-14 11:14:01 -0600

    Huesped Listo e inicio de reservacion

--- Reservacion/Reservacion.java
+++ Reservacion/Reservacion.java
@@ -1,5 +1,21 @@
 package Reservacion;
 
+import Huesped.Huesped;
+import Habitacion.Habitacion;
+
 public class Reservacion {
-    
+    private int idReservacion;
+    private Huesped huesped;
+    private Habitacion habitacion;
+    private int noches;
+
+    public Reservacion(Huesped huesped, Habitacion habitacion, int noches){
+        this.huesped = huesped;
+        this.habitacion = habitacion;
+        this.noches = noches;
+    }
+
+   
+
+
 }
